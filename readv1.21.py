ed883571be1afe8fb60266332a985b614f9bbf89
Turagon <52981127+Turagon@users.noreply.github.com>
2020-03-23 18:53:12 -0700

    Add files via upload

---
+++ readv1.21.py
@@ -0,0 +1,30 @@
+
+with open("reviews.txt", "r") as f :
+	review = [d for d in f]
+print(len(review))
+
+x = input("Please input how many reviews you want to print :")
+x = int(x)
+for ln in range(x) :
+	print(ln + 1, " ", review[ln])
+
+limit = input("Please input the limit length of each review you want to sort :")
+limit = int(limit)
+
+review_length = [len(d) for d in review]
+review_short = [d for d in review if len(d) < limit]
+review_long = [d for d in review if len(d) >= limit]
+review_good = [d for d in review if "good" in d]
+
+print(len(review_short))
+print(len(review_long))
+print(len(review_good))
+
+z = sum(review_length)/len(review)
+print(z)
+
+sum_length = 0
+for d in review :
+	sum_length += len(d)
+average = sum_length/len(review)
+print(average)
